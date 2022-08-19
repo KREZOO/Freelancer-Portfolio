7b3e7390b2bdcce4882c6f6ce611035b29066e31
KREZOO <111351989+KREZOO@users.noreply.github.com>
2022-08-19 17:45:57 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,6 +6,6 @@ navButton.onclick = () => {
     if (nav.classList.toggle('open')) {
         navButtonImage.src = 'img/icons/nav-close.svg';
     } else {
-        navButtonImage.src = '../img/icons/nav-open.png';
+        navButtonImage.src = 'https://cdn.discordapp.com/attachments/921436338316800010/1010197589687271454/nav-open.png';
     }
 }
